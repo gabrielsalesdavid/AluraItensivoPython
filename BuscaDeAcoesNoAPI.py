8e50129b4a0227f659914d79194be4943f9b3471
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-03-29 18:26:14 -0300

    Laço de repetição passando a linha com uma cor na posição [i]!

--- BuscaDeAcoesNoAPI.py
+++ BuscaDeAcoesNoAPI.py
@@ -40,3 +40,25 @@ df['Data'] = df['Data'].apply(mdates.date2num)
 # Crição da tela preferencial com altura e largura.
 fig, ax = plt.subplots(figsize =  (15, 8))
 
+# definição de layout do grafico candles.
+width = 0.7
+
+# laço de repetição para o grafico candles.
+for i in range(len(df)):
+    # estrutura de condições composta.
+    # se uma coluna declarda entre colchete for maior do que a outra, o comando retorna a cor verde no candles.
+    # do contrario vermelho.
+    if df['coluna'].iloc[i] > df['coluna'].iloc[i]:
+        color = 'green'
+    else:
+        color = 'red'
+
+    # define o ponto x e y da data em uma linha.
+    # define uma linha no valor minnimo e maximo.
+    ax.plot([df['Data'].iloc[i], df['Data'].iloc[i]],
+            [df['Minimo'].iloc[i], df['Maximo'].iloc[i]],
+            color = color, linewidth = 1)
+
+    # passa a cor da linha
+    ax.add_patch(plt.Rectangle((df['Data'].iloc[i] - width / 2, min(df['Abertura'].iloc[i], df['fechamento'].iloc[i])),
+                               width, abs(df['fechamento'].iloc[i] - df['Abertura'].iloc[i]), facecolor = color))
